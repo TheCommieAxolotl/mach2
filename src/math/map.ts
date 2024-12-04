3807e3e5b51f6b6762841a94aa4280c6e5ed0b19
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2024-12-04 20:56:50 +1100

    (math/map) add clamp

--- src/math/map.ts
+++ src/math/map.ts
@@ -4,3 +4,10 @@
 export const map = (x: number, inMin: number, inMax: number, outMin: number, outMax: number) => {
 	return ((x - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 };
+
+/**
+ * Clamp a number between a minimum and maximum value.
+ */
+export const clamp = (x: number, min: number, max: number) => {
+	return Math.min(Math.max(x, min), max);
+};
